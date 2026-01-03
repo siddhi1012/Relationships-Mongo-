c453a0352d4de1de671768a7dad7c2a1a2596c8b
Siddhi Gavhane <siddhigavhane03@gmail.com>
2026-01-04 00:50:13 +0530

    via upload

--- Models/post.js
+++ Models/post.js
@@ -20,29 +20,46 @@ const userSchema = new Schema({
 const postSchema = new Schema({
     content: String,
     likes: Number,
-    users: {
+    user: {
         type: Schema.Types.ObjectId,
         ref: "User"
     }
 });
 
 const User = mongoose.model("User", userSchema);
-const Post = mongoose.model("Customer", postSchema);
+const Post = mongoose.model("Post", postSchema);
 
-const addData = async () => {
-    let user1 = new User({
-        username: "Prasad",
-        email: "abc@gmail.com"
-    });
+// const addData = async () => {
 
-    let post1 = new Post({
-        content: "Bye Bye",
-        likes: 7,
-    });
-    post1.user = user1;
+//     // let user1 = new User({
+//     //     username: "Prasad",
+//     //     email: "abc@gmail.com"
+//     // });
 
-    await user1.save();
-    await post1.data();
+//     // let post1 = new Post({
+//     //     content: "Bye Bye",
+//     //     likes: 7,
+//     // });
+//     let user = await User.findOne({ username: "Prasad" });
+
+//     let post2 = new Post({
+//         content: "Hello World",
+//         likes: 4,
+//     });
+//     // post1.user  = user1;
+//     post2.user = user;
+
+//     //await user1.save();
+//     await post2.save();
+// }
+
+// addData();
+
+
+// Print information through it
+const getData = async () =>{
+    let result = await Post.findOne({}).populate("user");
+    console.log(result);
 }
 
-addData();
+getData();
